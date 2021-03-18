f7c0171fd744ab3ee707c94896f34f16175e29f8
Bipin Sawant <bipin.sawant@gmail.com>
2021-03-18 00:07:57 -0700

    telemetry appinsights

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -20,6 +20,7 @@ from opencensus.ext.azure.trace_exporter import AzureExporter
 from opencensus.trace.samplers import ProbabilitySampler
 from opencensus.trace.tracer import Tracer
 from opencensus.ext.flask.flask_middleware import FlaskMiddleware
+from applicationinsights import TelemetryClient
 
 # Logging
 # TODO: Setup logger
@@ -40,6 +41,7 @@ tracer = Tracer(
         connection_string = 'InstrumentationKey=7cc747bb-9a36-4c84-a232-265341000368'),
     sampler = ProbabilitySampler(1.0),
 )
+teleclient = TelemetryClient('7cc747bb-9a36-4c84-a232-265341000368')
 
 app = Flask(__name__)
 
@@ -89,10 +91,14 @@ def index():
         vote1 = r.get(button1).decode('utf-8')
         # TODO: use tracer object to trace cat vote
         tracer.span(name="CatsVotes")
+        teleclient.track_event("CatsVotes")
+        teleclient.flush()
 
         vote2 = r.get(button2).decode('utf-8')
         # TODO: use tracer object to trace dog vote
         tracer.span(name="DogsVotes")
+        teleclient.track_event("DogsVotes")
+        teleclient.flush()
 
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
